f2857660d4821380aa3d4ef57e9fc14704ed334b
OsvaldoGzz <osvaldo.gonzalezm@uanl.edu.mx>
2026-04-19 21:42:23 -0600

    Create HolaMundo.java
    agrego hola mundo

---
+++ Practica01/Ejercicios/src/HolaMundo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author moren
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+
+        System.out.println("Hola soy Osvaldo");
+        System.out.println("Mi matricula es 2043042");
+        System.out.println("Hoy es 6 de abril del 2026" );
+}
