bf0602c1a9caebb6d761f573078b5b6af848d649
OsvaldoGzz <osvaldo.gonzalezm@uanl.edu.mx>
2026-04-30 18:42:33 -0600

    Create Contacto.java
    Agrego clase Contacto serializable

---
+++ Practica09/src/Contacto.java
@@ -0,0 +1,33 @@
+package practica09;
+
+import java.io.Serializable;
+
+public class Contacto implements Serializable {
+
+    private String nombre;
+    private String telefono;
+    private String correo;
+
+    public Contacto(String nombre, String telefono, String correo) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " | " + telefono + " | " + correo;
+    }
+}
